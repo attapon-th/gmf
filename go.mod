d519b05c61c7ba8e8bb9176a664c5a2af6ee5d0e
Attapon Thanawong <attapon_srem@hotmail.com>
2022-07-05 10:12:49 +0700

    Deleted: services/cmd.go, services/replace_import.go Modified: go.mod, go.sum, helper/exec.go

--- go.mod
+++ go.mod
@@ -3,9 +3,15 @@ module github.com/attapon-th/gmf
 go 1.18
 
 require (
+	github.com/attapon-th/phuslulogger v1.0.1
+	github.com/phuslu/log v1.0.74
+)
+
+require (
 	github.com/alexflint/go-arg v1.0.0 // indirect
 	github.com/alexflint/go-scalar v1.0.0 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/rs/zerolog v1.15.0 // indirect
 	github.com/sirkon/go-imports-rename v0.1.3 // indirect
 	github.com/sirkon/gosrcfmt v1.6.0 // indirect
--- helper/exec.go
+++ helper/exec.go
@@ -41,11 +41,49 @@ func Exec(command string, args ...string) (output string, err error) {
 
 	out, err := cmd.Output()
 	if err != nil {
-		fmt.Printf(fullCommand, " ", strings.Join(commandArgs, " "))
+		// log.Error().Err(err).Msgf("%s %s", fullCommand, strings.Join(commandArgs, " "))
 		err = errors.New(stdErr.String())
 		return
 	}
 
 	output = strings.Trim(string(out), "\n")
 	return
+}
+
+// Exec cli commands
+// func ExecOutStd(command string, args ...string) (io.ReadCloser, error) {
+// 	commands := spaceRegexp.Split(command, -1)
+// 	command = commands[0]
+// 	commandArgs := []string{}
+// 	if len(commands) > 1 {
+// 		commandArgs = commands[1:]
+// 	}
+// 	if len(args) > 0 {
+// 		commandArgs = append(commandArgs, args...)
+// 	}
+
+// 	fullCommand, err := exec.LookPath(command)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("%s cannot be found", command)
+// 	}
+
+// 	cmd := exec.Command(fullCommand, commandArgs...)
+// 	cmd.Env = os.Environ()
+// 	cmd.Stderr = cmd.Stdout
+// 	stdOut, err := cmd.StdoutPipe()
+// 	if err != nil {
+// 		return stdOut, err
+// 	}
+// 	if err := cmd.Run(); err != nil {
+// 		return stdOut, err
+// 	}
+// 	return stdOut, nil
+// }
+
+func ConcatError(err ...error) error {
+	errStr := ""
+	for _, err := range err {
+		errStr += err.Error()
+	}
+	return errors.New(errStr)
 }
