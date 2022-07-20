f3e53929c1efed976c9356aa40b275e8aae1abef
Attapon Thanawong <attapon_srem@hotmail.com>
2022-07-20 18:17:59 +0700

    Modified: Taskfile.yaml, cmd/gmf/gmf.go

--- cmd/gmf/gmf.go
+++ cmd/gmf/gmf.go
@@ -7,7 +7,7 @@ import (
 var (
 	AppName   string = "gmf"
 	Version   string = "2.0.5"
-	Build     string = "810be8302b422a60c75f7c19a69e6e18032dda17"
+	Build     string = "bd7529e2f3a48cdb5bf7a41cdd3a6551a0d46cd4"
 	DateBuild string = "2022-07-20T17:30:42+07:00"
 )
 
